6da4315bafe65decc4beb25afdafc6f59a41143f
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2023-02-09 16:04:40 +0200

    fix: Cache::tags()->flush() clearing the whole cache

--- tests/Integration/FailSafeRepositoryTest.php
+++ tests/Integration/FailSafeRepositoryTest.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Support\Facades\Log;
 use TenantCloud\LaravelBetterCache\FailSafe\FailSafeRepository;
 use Tests\TestCase;
-use TiMacDonald\Log\LogEntry;
 use TiMacDonald\Log\LogFake;
 
 /**
@@ -21,284 +20,257 @@ class FailSafeRepositoryTest extends TestCase
 	{
 		parent::setUp();
 
+		Log::swap(new LogFake());
+
 		$this->store = cache()->store('redis_fail_safe');
+		$this->store->clear();
+	}
+
+	protected function tearDown(): void
+	{
+		Log::assertNothingLogged();
+
+		parent::tearDown();
 	}
 
 	public function testHas(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 'asd');
 
-		self::assertFalse($this->store->has('test'));
-		self::assertTrue($this->store->missing('test'));
+		self::assertTrue($this->store->has('test1'));
+		self::assertFalse($this->store->missing('test1'));
 
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to check if item exists in cache.',
-			2
-		);
+		self::assertFalse($this->store->has('test2'));
+		self::assertTrue($this->store->missing('test2'));
 	}
 
 	public function testGet(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 'asd');
 
-		self::assertNull($this->store->get('test'));
-		self::assertSame(123, $this->store->get('test', 123));
-		self::assertSame(456, $this->store->get('test', fn () => 456));
+		self::assertSame('asd', $this->store->get('test1'));
 
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to get items from cache.',
-			3
-		);
+		self::assertNull($this->store->get('test2'));
+		self::assertSame('asd', $this->store->get('test2', 'asd'));
+		self::assertSame('qwe', $this->store->get('test2', fn () => 'qwe'));
 	}
 
 	public function testMany(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 'asd');
 
 		self::assertSame([
-			'test1' => 123,
-			'test2' => 456,
+			'test1' => 'asd',
+			'test'  => null,
+		], $this->store->many([
+			'test1',
+			'test',
+		]));
+
+		self::assertSame([
+			'test1' => 'asd',
+			'test2' => 'qwe',
 			'test3' => null,
 			'test4' => null,
 			'test5' => null,
 			'test6' => null,
 		], $this->store->many([
-			'test1' => 123,
-			'test2' => fn () => 456,
+			'test1' => 'asd',
+			'test2' => fn () => 'qwe',
 			'test3',
 			'test4',
 			'test5',
 			'test6',
 		]));
 		self::assertSame([
-			'test7' => 123,
+			'test7' => 'asd',
 		], $this->store->getMultiple([
 			'test7',
-		], 123));
-
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to get items from cache.',
-			2
-		);
+		], 'asd'));
 	}
 
 	public function testPut(): void
 	{
-		Log::swap($log = new LogFake());
+		self::assertTrue($this->store->put('test1', 'asd1', CarbonInterval::minute()));
+		self::assertTrue($this->store->set('test2', 'asd2', CarbonInterval::minute()));
+		self::assertTrue($this->store->put('test3', 'expired', CarbonInterval::second()));
 
-		self::assertFalse($this->store->put('test', 'asd', CarbonInterval::minute()));
-		self::assertFalse($this->store->set('test', 'asd', CarbonInterval::minute()));
+		sleep(2);
 
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to put items into cache.',
-			2
-		);
+		self::assertSame('asd1', $this->store->get('test1'));
+		self::assertSame('asd2', $this->store->get('test2'));
+		self::assertNull($this->store->get('test3'));
 	}
 
 	public function testPutMany(): void
 	{
-		Log::swap($log = new LogFake());
+		self::assertTrue($this->store->putMany([
+			'test1' => 'asd1',
+			'test2' => 'asd2',
+			'test3' => null,
+		], CarbonInterval::minute()));
+		self::assertTrue($this->store->setMultiple([
+			'test2' => 'expired',
+		], CarbonInterval::second()));
 
-		self::assertFalse($this->store->putMany([
-			'test1' => null,
+		sleep(2);
+
+		self::assertSame([
+			'test1' => 'asd1',
 			'test2' => null,
 			'test3' => null,
-			'test4' => null,
-			'test5' => null,
-			'test6' => null,
-		], CarbonInterval::minute()));
-		self::assertFalse($this->store->setMultiple([
-			'test7' => null,
-		], CarbonInterval::minute()));
-
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to put items into cache.',
-			2
-		);
+		], $this->store->many([
+			'test1',
+			'test2',
+			'test3',
+		]));
 	}
 
 	public function testForever(): void
 	{
-		Log::swap($log = new LogFake());
+		self::assertTrue($this->store->forever('test', 'asd'));
 
-		self::assertFalse($this->store->forever('test', 'asd'));
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to put items into cache.'
-		);
+		self::assertSame('asd', $this->store->get('test'));
 	}
 
 	public function testAdd(): void
 	{
-		Log::swap($log = new LogFake());
+		self::assertTrue($this->store->add('test', 'asd', CarbonInterval::minute()));
+		self::assertFalse($this->store->add('test', 'other value', CarbonInterval::minute()));
 
-		self::assertFalse($this->store->add('test', 'asd', CarbonInterval::minute()));
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to add items into cache.'
-		);
+		self::assertSame('asd', $this->store->get('test'));
 	}
 
 	public function testRemember(): void
 	{
-		Log::swap($log = new LogFake());
+		self::assertSame('asd', $this->store->remember('test1', CarbonInterval::minute(), fn () => 'asd'));
+		self::assertSame('qwe', $this->store->remember('test2', CarbonInterval::second(), fn () => 'qwe'));
 
-		self::assertSame(123, $this->store->remember('test', CarbonInterval::minute(), fn () => 123));
+		sleep(2);
 
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to remember items into cache.'
-		);
+		self::assertSame('asd', $this->store->get('test1'));
+		self::assertNull($this->store->get('test2'));
 	}
 
 	public function testRememberForever(): void
 	{
-		Log::swap($log = new LogFake());
+		self::assertSame('asd', $this->store->rememberForever('test', fn () => 'asd'));
 
-		self::assertSame(123, $this->store->rememberForever('test', fn () => 123));
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to remember items into cache.'
-		);
+		self::assertSame('asd', $this->store->get('test'));
 	}
 
 	public function testPull(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->set('test1', 'asd');
 
-		self::assertNull($this->store->pull('test'));
-		self::assertSame(123, $this->store->pull('test', 123));
-		self::assertSame(456, $this->store->pull('test', fn () => 456));
+		self::assertSame('asd', $this->store->pull('test1'));
+		self::assertNull($this->store->pull('test1'));
+		self::assertNull($this->store->pull('test2'));
 
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to pull items from cache.',
-			3
-		);
+		self::assertSame('asd', $this->store->pull('test3', 'asd'));
+		self::assertSame('qwe', $this->store->pull('test3', fn () => 'qwe'));
 	}
 
 	public function testIncrement(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 3);
 
-		self::assertFalse($this->store->increment('test'));
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to increment key in cache.'
-		);
+		self::assertSame(4, $this->store->increment('test1'));
+		self::assertSame(1, $this->store->increment('test2'));
 	}
 
 	public function testDecrement(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 3);
 
-		self::assertFalse($this->store->decrement('test'));
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to decrement key in cache.'
-		);
+		self::assertSame(2, $this->store->decrement('test1'));
+		self::assertSame(-1, $this->store->decrement('test2'));
 	}
 
 	public function testForget(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 'asd');
+		$this->store->put('test2', 'asd');
 
-		self::assertFalse($this->store->forget('test'));
-		self::assertFalse($this->store->delete('test'));
+		self::assertTrue($this->store->forget('test1'));
+		self::assertTrue($this->store->delete('test2'));
 
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to forget items from cache.',
-			2
-		);
+		self::assertNull($this->store->get('test1'));
+		self::assertNull($this->store->get('test2'));
 	}
 
 	public function testForgetMany(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test1', 'asd');
+		$this->store->put('test2', 'asd');
+		$this->store->put('test3', 'asd');
+
+		self::assertTrue($this->store->deleteMultiple([
+			'test1',
+			'test2',
+		]));
 
 		self::assertFalse($this->store->deleteMultiple([
+			'test3',
+			'test4',
+		]));
+
+		self::assertSame([
+			'test1' => null,
+			'test2' => null,
+			'test3' => null,
+			'test4' => null,
+		], $this->store->many([
 			'test1',
 			'test2',
 			'test3',
 			'test4',
-			'test5',
-			'test6',
 		]));
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to forget items from cache.'
-		);
 	}
 
 	public function testFlush(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test', 'asd');
 
-		self::assertFalse($this->store->flush());
-		self::assertFalse($this->store->clear());
+		self::assertTrue($this->store->flush());
 
-		$log->assertLoggedTimes(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to flush cache.',
-			2
-		);
+		self::assertFalse($this->store->has('test'));
+		$this->store->put('test', 'asd');
+
+		self::assertTrue($this->store->clear());
+
+		self::assertFalse($this->store->has('test'));
 	}
 
 	public function testFlushStale(): void
 	{
-		Log::swap($log = new LogFake());
-
 		$this->store->flushStale();
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to flush stale cache.'
-		);
 	}
 
 	public function testTagList(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->tags('test1', 'test2')->put('key', 'asd');
 
-		self::assertEmpty($this->store->tagList());
-
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to get tag list from cache.'
-		);
+		self::assertSame(['test1', 'test2'], $this->store->tagList()->all());
 	}
 
 	public function testTags(): void
 	{
-		Log::swap($log = new LogFake());
+		$this->store->put('test', 'asd');
 
 		self::assertNull($this->store->tags('one')->get('test'));
-		self::assertFalse($this->store->tags('one')->put('test', '123'));
-		self::assertEmpty($this->store->tags('one')->entries());
 
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to get items from cache.'
-		);
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to put items into cache.'
-		);
-		$log->assertLogged(
-			fn (LogEntry $log) => $log->level === 'error' &&
-				$log->message === 'Failed to get entries list from cache.'
-		);
+		$this->store->tags('one')->put('test', 'asd1');
+		$this->store->tags('two')->put('test', 'asd2');
+
+		self::assertSame('asd', $this->store->get('test'));
+		self::assertSame('asd1', $this->store->tags('one')->get('test'));
+		self::assertSame('asd2', $this->store->tags('two')->get('test'));
+
+		$this->store->tags('one')->flush();
+
+		self::assertSame('asd', $this->store->get('test'));
+		self::assertNull($this->store->tags('one')->get('test'));
+		self::assertSame('asd2', $this->store->tags('two')->get('test'));
 	}
 }
